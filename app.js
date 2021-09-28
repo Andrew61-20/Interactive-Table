0155594f5abccf0fb16d3761fb68b4703e6899c4
aartemenko1 <aartemenko1@exadel.com>
2021-09-28 19:46:41 +0300

    commit2

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const MongoClient = require("mongodb").MongoClient;
 const config = require('config');
+const path = require('path');
 const app = express();
 app.use(express.json({extended: true}))
 const PORT = config.get('port') || 5000
   
 const mongoClient = new MongoClient("mongodb+srv://andrey1:aa610102@cluster0.doumg.mongodb.net/sample_supplies?retryWrites=true&w=majority");
 app.use(express.static(__dirname + "/public"));
+
+if(process.NODE_ENV === 'production') {
+    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+
 
     mongoClient.connect(function(err, client){
         if(err) return console.log(err);
