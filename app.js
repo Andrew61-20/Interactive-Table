d675712fc6edcc07f5a2eb501190371fa82dc1da
aartemenko1 <aartemenko1@exadel.com>
2021-09-28 21:08:18 +0300

    commit5

--- app.js
+++ app.js
@@ -9,22 +9,20 @@ const PORT = config.get('port') || 5000
 const mongoClient = new MongoClient("mongodb+srv://andrey1:aa610102@cluster0.doumg.mongodb.net/sample_supplies?retryWrites=true&w=majority");
 app.use(express.static(__dirname + "/public"));
 
-if(process.NODE_ENV === 'production') {
-    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+// if(process.NODE_ENV === 'production') {
+//     app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+//     })
+// }
 
-
-
-    mongoClient.connect(function(err, client){
-        if(err) return console.log(err);
-        app.locals.collection = client.db("sample_supplies").collection("job");
-        app.listen(PORT, function(){
-            console.log(`App has been started on port ${PORT}...`);
-        });
+mongoClient.connect(function(err, client){
+    if(err) return console.log(err);
+    app.locals.collection = client.db("sample_supplies").collection("job");
+    app.listen(PORT, function(){
+        console.log(`App has been started on port ${PORT}...`);
     });
+});
 
 app.get("/api/jobs", function (req, res) {
     const job = app.locals.collection.find({}).toArray(function(err, jobs){
